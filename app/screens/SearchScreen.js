4d5253182f084dc2f10a02996829b9f1f70b0c74
Hamed <hamed.jenabi@gmail.com>
2020-06-25 14:04:37 +0200

    LOGIN Woring

--- app/screens/SearchScreen.js
+++ app/screens/SearchScreen.js
@@ -32,17 +32,13 @@ export default function HomeScreen() {
 
   const onPressAction = () => {
     setShow(true);
-    fetch(
-      `https://petapro-translate-v1.p.rapidapi.com/?langpair=en-de&query=${word}&wortart=%7Bwordclass%7D`,
-      {
-        method: 'GET',
-        headers: {
-          'x-rapidapi-host': 'petapro-translate-v1.p.rapidapi.com',
-          'x-rapidapi-key':
-            '0a868878b3msha696a9ae4c53aaep18f96fjsn41fe05117014',
-        },
+    fetch(`KEX`, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'petapro-translate-v1.p.rapidapi.com',
+        'x-rapidapi-key': '0a868878b3msha696a9ae4c53aaep18f96fjsn41fe05117014',
       },
-    )
+    })
       .then((res) => res.json())
       .then((response) => {
         setLoading(false);
